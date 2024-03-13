7ca387433a3e838888010126324a6e4370545b23
a-green-hand-jack <green-hand-jack@users.noreply.github.com>
2024-03-13 21:06:38 +0800

    失败，可能式设备的原因？计算的好慢。似乎就算是同一个chr最好做一个分割不过发现可以通过一点重叠来解决分割的问题。

--- dataset/load_large_dataset.py
+++ dataset/load_large_dataset.py
@@ -1,56 +1,48 @@
-from transformers import LineByLineTextDataset
-from transformers import RobertaTokenizerFast
-from datasets import load_dataset
-import itertools
-from torch.utils.data import random_split
+import pandas as pd
+from tqdm import tqdm
+import random
+import os
 
-tokenizer_path = "../tokenizer/save_tokenizer_small"
-txt_file_path = "../../../Datasets/Human_genome/huixin/24_chromosomes-002-small.txt"
-print(f"从{tokenizer_path}中加载tokenizer")
-print(f"从{txt_file_path}中加载数据")
-# 现在在teansformers中重新创建tokenizer
-tokenizer = RobertaTokenizerFast.from_pretrained(tokenizer_path, max_len=512)
+def write_sequences_to_files(df, folder_path):
+    # 创建文件夹路径
+    os.makedirs(folder_path, exist_ok=True)
+    
+    # 使用 tqdm 创建进度条，并应用到 DataFrame 的每一行
+    tqdm.pandas(desc="Writing sequences")
+    
+    # 定义处理每一行的函数
+    def process_row(row):
+        # 提取 ID 和 Sequence
+        sequence_id = row['ID']
+        sequence = row['Sequence']
 
-# dataset = LineByLineTextDataset(
-#     tokenizer=tokenizer,
-#     file_path="../../../Datasets/Human_genome/huixin/24_chromosomes-002-small.txt",
-#     block_size=128,
-# )
+        # 直接删除序列中的所有 "N"
+        sequence = sequence.replace("N", "")
 
-# # 获取数据集中的前几个示例
-# num_examples_to_check = 1
-# for i in range(num_examples_to_check):
-#     example = dataset[i]
-#     print(example)
-#     tokens = tokenizer.convert_ids_to_tokens(example["input_ids"])
-#     decoded_text = tokenizer.decode(example['input_ids'], skip_special_tokens=False ,return_tensor='pt')
-#     print("Tokenized Text: ", tokens)
-#     print("After decoded text: ",decoded_text)
+        # 构建文件名
+        file_name = f"{sequence_id}.txt"
 
-pubmed_dataset_streamed = load_dataset(
-    "text", data_files=txt_file_path, streaming=True
-)
-print(pubmed_dataset_streamed)
-print(next(iter(pubmed_dataset_streamed)))
-tokenized_dataset = pubmed_dataset_streamed.map(lambda x: tokenizer(x["text"]))
-print(next(iter(tokenized_dataset)))
+        # 写入文件
+        with open(os.path.join(folder_path, file_name), 'w') as file:
+            file.write(sequence)
+        
+    # 应用进度条到 DataFrame 的每一行
+    df.progress_apply(process_row, axis=1)
 
-# dataset_head = pubmed_dataset_streamed.take(5)
-# print(list(dataset_head))
-# 通过 islice 获取前几个样本
-# 获取数据集的一个子集
-dataset = pubmed_dataset_streamed['train']
+def main(csv_file_path:str = "", txt_output_path: str = ""):
 
-# 划分数据集
-dataset_len = dataset.info['start'] - dataset.info['end']
-train_size = int(0.8 * len(dataset))
-eval_size = len(dataset) - train_size
-train_dataset, eval_dataset = random_split(dataset, [train_size, eval_size])
+    dataframe = pd.read_csv(csv_file_path)
+    # print(dataframe.head())
+    print(f"Shape of the DataFrame: {dataframe.shape}")
+    write_sequences_to_files(dataframe, txt_output_path)
+    return dataframe
 
-# 获取前几个样本
-num_samples = 5
-samples = list(itertools.islice(dataset, num_samples))
 
-# 打印样本内容
-for sample in samples:
-    print(sample)
+if __name__ == "__main__":
+    csv_file_path = "../../Datasets/Human_genome/huixin/24_chromosomes-002.csv"
+    txt_output_path = "../../Datasets/Human_genome/huixin/24-txt/"
+    # small_path = "../../Datasets/Human_genome/huixin/24_chromosomes-002.txt"
+
+    data_csv = main(csv_file_path = csv_file_path, txt_output_path =txt_output_path)
+
+    
--- tokenizer/tokenizer copy.py
+++ tokenizer/tokenizer copy.py
@@ -3,50 +3,46 @@ from tokenizers import (
     pre_tokenizers,
     trainers,
     Tokenizer,
+    ByteLevelBPETokenizer
 )
+
+import datasets
+from datasets import load_dataset
 import os
 from tqdm import tqdm
 import shutil
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+            
+def train_tokenizer(tokenizer, sequence, trainer, chunk_size=1000, overlap=100):
+    start = 0
+    end = chunk_size
+    chunks = []
+    while start < len(sequence):
+        chunk = sequence[start:end]
+        chunks.append(chunk)
+        start += chunk_size - overlap
+        end = min(start + chunk_size, len(sequence))
+    chunks_number = len(chunks)
+    print(f"分割之后的列表的长度是{chunks_number}")
+    tokenizer.train_from_iterator(chunks, trainer=trainer,length=chunks_number)
 
-def split_large_file(input_file, output_dir, chunk_size:int =100000000):
-    print(f"开始分隔文件{input_file}，并且保存临时文件夹{output_dir}")
-    with open(input_file, 'r') as file:
-        content = file.read()
 
-    total_chunks = int(len(content) / chunk_size) + 1
-    chunks = [content[i:i + chunk_size] for i in range(0, len(content), chunk_size)]
 
-    os.makedirs(output_dir, exist_ok=True)
-
-    for i, chunk in tqdm(enumerate(chunks), total=total_chunks, desc="Processing Chunks", unit="chunk"):
-        output_file = os.path.join(output_dir, f"chunk_{i + 1}.txt")
-        with open(output_file, 'w') as output:
-            output.write(chunk)
-
-def train_tokenizer(tokenizer, lines, trainer, report_interval=10000):
-    line_count = 0
-    chunk = []
-    for line in lines:
-        line_count += 1
-        chunk.append(line.strip())
-        if len(chunk) == report_interval:
-            tokenizer.train_from_iterator(chunk, trainer=trainer)
-            chunk = []
-    if chunk:
-        tokenizer.train_from_iterator(chunk, trainer=trainer)
-
-def train_tokenizer_on_chunks(tokenizer, chunks_dir, trainer, report_interval=10000):
-    line_count = 0
+def train_tokenizer_on_chunks(tokenizer, chunks_dir, trainer, chunk_size=10000000, overlap=512):
 
     for file_name in tqdm(os.listdir(chunks_dir), desc="Processing Files", unit="file"):
+        
         if file_name.endswith(".txt"):
             file_path = os.path.join(chunks_dir, file_name)
             with open(file_path, 'r') as file:
-                lines = [line.strip() for line in file]
-                train_tokenizer(tokenizer, lines, trainer=trainer, report_interval=report_interval)
-                line_count += len(lines)
+                sequence = file.read().strip()
+                tqdm_desc = f"Processing {file_name} (Length: {len(sequence)})"
+                tqdm.write(tqdm_desc)  # 更新进度条描述
+                train_tokenizer(tokenizer, sequence, trainer, chunk_size, overlap)
+
+
+
 
 def save_tokenizer(tokenizer, save_path):
     os.makedirs(save_path, exist_ok=True)
@@ -61,8 +57,9 @@ def save_tokenizer(tokenizer, save_path):
 def main(data_path:str = "",
          save_path:str = "",
          special_tokens:list = [],
-         vocab_size:int = 0,
-         chunk_size:int = 0):
+         vocab_size:int = int(2^8),
+         chunk_size:int = int(2^30),
+         overlap:int = int(2^12)):
     # 初始化
     tokenizer = Tokenizer(models.BPE())
     # pre-tokenization
@@ -71,33 +68,31 @@ def main(data_path:str = "",
     # 训练分词器
     print(f"增加的特殊token有：{special_tokens}")
     print(f"增加了一下指导性设定：字典的大小：{vocab_size}")
+    print(f"一次批次的长度是{chunk_size}")
+    print(f"重叠的长度是{overlap}")
     trainer = trainers.BpeTrainer(
         vocab_size=vocab_size,
         special_tokens=special_tokens,
         show_progress=False,
     )
-    # 分割大文件
-    temp_folder = "./chunks"
-    split_large_file(data_path, temp_folder, chunk_size)
-
     # 训练分词器
-    train_tokenizer_on_chunks(tokenizer, temp_folder, trainer)
+    train_tokenizer_on_chunks(tokenizer, data_path, trainer, chunk_size, overlap)
     # 保存分词器
     save_tokenizer(tokenizer, save_path)
-    # 在程序结束时删除临时文件夹及其内容
-    shutil.rmtree(temp_folder)
-    print(f"Temporary folder {temp_folder} deleted")
+
 
 if __name__ == "__main__":
-    data_path:str = "../../Datasets/Human_genome/huixin/24_chromosomes-002.txt"
+    data_path:str = "../../Datasets/Human_genome/huixin/24-txt/"
     save_path:str = "./my_tokenizer/"
     special_tokens = ["[PAD]", "[CLS]", "[SEP]", "[MASK]", "[UNK]"]
     vocab_size:int = 2 ** 12
-    chunk_size:int = 10000000
+    chunk_size:int = 2 ** 10
+    overlap:int = 2 ** 6
 
     main(data_path=data_path, 
          save_path=save_path, 
          special_tokens=special_tokens, 
          vocab_size=vocab_size, 
-         chunk_size=chunk_size
+         chunk_size=chunk_size,
+         overlap=overlap
          )
