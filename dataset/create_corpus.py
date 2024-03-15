1258d6205e223122692f50050dc683b87485f6c1
a-green-hand-jack <green-hand-jack@users.noreply.github.com>
2024-03-15 21:51:55 +0800

    加入GENA_LM

---
+++ dataset/create_corpus.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+import ast
+import gzip
+import json
+import random
+from pathlib import Path
+from typing import Generator, List, Literal, Optional, Tuple
+
+import click
+import pandas as pd
+from Bio import Seq, SeqIO
+from Bio.SeqRecord import SeqRecord
+from tqdm import tqdm
+import concurrent.futures
+
+# reproducible randomness
+random.seed(42)
+
+
+class Document:
+    def __init__(
+        self, sentences: Optional[List[str]] = None, metadata: Optional[dict] = None
+    ) -> None:
+        """
+        Data structure to store subsequences that are forming the chromosome
+
+        Each document contains up to `max(sentence_bound) * max(lengths_bound)` of nts
+        """
+        if sentences is None:
+            self.sentences = []
+        else:
+            self.sentences = sentences
+        self.metadata = metadata
+
+    def append(self, sentence: str) -> None:
+        self.sentences.append(sentence)
+
+    def to_text(self) -> str:
+        return "\n".join(self.sentences)
+
+    def to_jsonl(self) -> None:
+        if self.metadata:
+            return json.dumps({"text": self.sentences, **self.metadata})
+        else:
+            return json.dumps({"text": self.sentences})
+
+
+def generate_documents(
+    chr_sequence: Seq,
+    sentences_bounds: Tuple[int] = (50, 100),
+    lenghts_bounds: Tuple[int] = (500, 1000),
+) -> Generator[Document, None, None]:
+    """
+    From a single chromosome yield a set of documents that cover that chromosome.
+    This operation is done ten-fold. 
+    """
+
+    C = len(chr_sequence)  # chromosome length
+
+    for _ in range(10):
+        q = random.randint(0, 5000)  # random start position from the 5' end
+        while q < C:
+            s = random.randint(*sentences_bounds)  # number of sentences per document
+            d = Document()
+            for _ in range(s):
+                l = random.randint(*lenghts_bounds)  # length of each sentence
+                if q+l > C:
+                    l = C - q
+                d.append(str(chr_sequence[q : q + l]).upper())
+                q += l  # update position for the new sentence
+                if q>=C: # we reached the end of seq, no more sentences available
+                    break
+            yield d
+
+
+def handle_chromosome(
+    chr: SeqIO.SeqRecord,
+    outdir: Path,
+    io_mode: Literal["single_txt", "jsonl", "multiple_txt"] = "single_txt",
+):
+    """
+    For a given chromosome make documents and write them to corresponding files
+    """
+
+    if io_mode == "single_txt":
+        filename = outdir / f"{chr.name}_documents.txt"
+        with filename.open(mode="w") as out:
+            for document in generate_documents(chr.seq):
+                out.write(document.to_text())
+                out.write("\n")
+    elif io_mode == "jsonl":
+        filename = outdir / f"{chr.name}_documents.jsonl"
+        with open(filename, mode="w") as out:
+            for document in generate_documents(chr.seq):
+                out.write(document.to_jsonl())
+                out.write("\n")
+    elif io_mode == "multiple_txt":
+        for idx, document in enumerate(generate_documents(chr.seq)):
+            filename = outdir / f"{chr.name}_document_{idx}.txt"
+            with filename.open(mode="w") as out:
+                out.write(document.to_text())
+
+
+def read_single_fasta(fna_file: Path, output_dir: Optional[Path] = None,
+                      contigs_split_file: Optional[Path] = None,
+                      io_mode: Literal["single_txt", "jsonl", "multiple_txt"] = "single_txt",
+                      min_len: Optional[int] = 10000,
+                      rc: Optional[bool] = False):
+    if not output_dir:
+        output_dir = Path(".")
+
+    if contigs_split_file is not None:
+        contigs = pd.read_csv(contigs_split_file, dtype=str)
+        if len(contigs) == 0: # no contig split data available
+            contigs_split_file = None
+        else:
+            contigs = contigs.set_index("name")
+            contigs["intervals"] = contigs["intervals"].apply(ast.literal_eval)
+
+    if fna_file.endswith(".gz"):
+        fasta_open = gzip.open
+    else:
+        fasta_open = open
+    
+    # 设置一个计数器用于记录处理的染色体数量
+    chromosome_count = 0
+    
+    with fasta_open(fna_file,"rt") as input_handle:
+        for record in tqdm(SeqIO.parse(input_handle, "fasta")):
+            # 判断是否达到指定的染色体数量
+            if chromosome_count >= 24:
+                break  # 如果达到，则退出循环
+            
+            if rc:
+                record.name  = "rc_" + record.name # modify record name to have different names of rc-files later
+            if ("mitochondrion" not in record.description) and len(record.seq) >= min_len:
+                if contigs_split_file is not None:
+                    chr_contigs = contigs.loc[record.id,"intervals"]
+                    for contig in chr_contigs:
+                        contig_start = contig[0]
+                        contig_end = contig[1]
+                        if contig_end-contig_start < min_len:
+                            continue
+                        contig_name = "_c"+str(contig_start)+"_"+str(contig_end)
+                        if rc:
+                            seq = record.seq[contig_start:contig_end]
+                        else:
+                            seq = Seq.reverse_complement(record.seq[contig_start:contig_end])
+                        seq_record = SeqRecord(seq = seq,
+                                                id = record.id+contig_name,
+                                                name = record.name+contig_name,
+                                                description = record.description+contig_name)
+                        handle_chromosome(seq_record, outdir=output_dir, io_mode=io_mode)
+                else:
+                    if rc:
+                        record.seq = record.seq.reverse_complement()
+                    handle_chromosome(record, outdir=output_dir, io_mode=io_mode)
+            
+            # 每处理完一个染色体，计数器加 1
+            chromosome_count += 1
+
+
+# example usage:
+# python create_corpus.py --input-file ./ncbi_dataset/data/GCA_009914755.4/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna \
+#  --output-dir data/processed/human/ --io-mode jsonl --min-len 10000
+
+@click.command()
+@click.option("--input-file", type=str)
+@click.option("--contigs-split-file", type=str)
+@click.option("--output-dir", type=click.Path(path_type=Path, dir_okay=True))
+@click.option("--io-mode", type=click.Choice(["single_txt", "jsonl", "multiple_txt"]), default="single_txt")
+@click.option("--min-len", type=click.INT, default=10000, help="Minimum contig length to be included")
+#@click.option("--rc", is_flag=True, show_default=True, default=False, help="Reverse-complement all sequences")
+def cli(input_file, contigs_split_file, output_dir, io_mode, min_len):
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    # 创建 ThreadPoolExecutor 对象，指定线程池中的线程数量
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # 提交第一个函数调用给线程池，并返回 Future 对象
+        future1 = executor.submit(read_single_fasta, input_file, contigs_split_file=contigs_split_file, 
+                                   output_dir=output_dir, io_mode=io_mode,
+                                   min_len=min_len, rc=True)
+        # 提交第二个函数调用给线程池，并返回 Future 对象
+        future2 = executor.submit(read_single_fasta, input_file, contigs_split_file=contigs_split_file, 
+                                   output_dir=output_dir, io_mode=io_mode,
+                                   min_len=min_len, rc=False)
+        
+        # 等待两个函数调用完成
+        concurrent.futures.wait([future1, future2])
+
+if __name__ == "__main__":
+
+    """
+    在这里给出一个实例：
+    python create_corpus.py --input-file "../../Datasets/Human_genome/huixin/GRCh38.p14.genome.fa.gz" --output-dir "../../Datasets/Human_genome/huixin/create_corpus"      
+    """
+    cli()
--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,103 +1,60 @@
-from tokenizers import (
-    models,
-    pre_tokenizers,
-    trainers,
-    Tokenizer,
-)
-import os
-from tqdm import tqdm
-import shutil
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
+import itertools
+from pathlib import Path
+from typing import Optional
+import click
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
 
-def split_large_file(input_file, output_dir, chunk_size:int =100000000):
-    print(f"开始分隔文件{input_file}，并且保存临时文件夹{output_dir}")
-    with open(input_file, 'r') as file:
-        content = file.read()
+DEFAULT_VOCAB_SIZE = 32_000
 
-    total_chunks = int(len(content) / chunk_size) + 1
-    chunks = [content[i:i + chunk_size] for i in range(0, len(content), chunk_size)]
 
-    os.makedirs(output_dir, exist_ok=True)
+def create_bpe_tokenizer(
+    input_dir: Path,
+    output_dir: Path,
+    tokenizer_name: str = "BPE",
+    limit_files: Optional[int] = None,
+    vocab_size: int = DEFAULT_VOCAB_SIZE,
+) -> None:
+    """
+    Train basic BPE tokenizer without preprocessing with BERT-like special tokens
 
-    for i, chunk in tqdm(enumerate(chunks), total=total_chunks, desc="Processing Chunks", unit="chunk"):
-        output_file = os.path.join(output_dir, f"chunk_{i + 1}.txt")
-        with open(output_file, 'w') as output:
-            output.write(chunk)
+    Reads all .txt files in given directory, saves
+    """
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+    trainer = BpeTrainer(vocab_size = vocab_size, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
 
-def train_tokenizer(tokenizer, lines, trainer, report_interval=10000):
-    line_count = 0
-    chunk = []
-    for line in lines:
-        line_count += 1
-        chunk.append(line.strip())
-        if len(chunk) == report_interval:
-            tokenizer.train_from_iterator(chunk, trainer=trainer)
-            chunk = []
-    if chunk:
-        tokenizer.train_from_iterator(chunk, trainer=trainer)
+    files = input_dir.glob("*.txt")
+    if limit_files is not None or limit_files != 0:
+        files = itertools.islice(files, 0, limit_files)
 
-def train_tokenizer_on_chunks(tokenizer, chunks_dir, trainer, report_interval=10000):
-    line_count = 0
+    tokenizer.train([str(file) for file in files], trainer)
+    tokenizer.save(str((output_dir / f"{tokenizer_name}.json").resolve()))
 
-    for file_name in tqdm(os.listdir(chunks_dir), desc="Processing Files", unit="file"):
-        if file_name.endswith(".txt"):
-            file_path = os.path.join(chunks_dir, file_name)
-            with open(file_path, 'r') as file:
-                lines = [line.strip() for line in file]
-                train_tokenizer(tokenizer, lines, trainer=trainer, report_interval=report_interval)
-                line_count += len(lines)
 
-def save_tokenizer(tokenizer, save_path):
-    os.makedirs(save_path, exist_ok=True)
-    print(f"Creating directory: {save_path}")
-    save_path = os.path.join(save_path, "config.json")
-    tokenizer.save(save_path) # 这种保存方式得到是一个json文件，虽然也包括了一切需要的信息，但是不能直接被RobertaTokenizerFast.from_pretrained 接受,但是可以使用：
-
-    # tokenizer.model.save(save_path) # 这种方法保存是一个merges.txt+vocab.json，可以被RobertaTokenizerFast.from_pretrained直接接受，采用下面这种方法加载：
-
-    
-
-def main(data_path:str = "",
-         save_path:str = "",
-         special_tokens:list = [],
-         vocab_size:int = 0,
-         chunk_size:int = 0):
-    # 初始化
-    tokenizer = Tokenizer(models.BPE())
-    # pre-tokenization
-    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
-
-    # 训练分词器
-    print(f"增加的特殊token有：{special_tokens}")
-    print(f"增加了一下指导性设定：字典的大小：{vocab_size}")
-    trainer = trainers.BpeTrainer(
+@click.command()
+@click.option("--input_dir", type=click.Path(path_type=Path, dir_okay=True))
+@click.option("--output_dir", type=click.Path(path_type=Path, dir_okay=True))
+@click.option("--limit-files", type=int, )
+@click.option("--vocab-size", type=int, default=DEFAULT_VOCAB_SIZE, show_default=True)
+@click.option("--tokenizer_name", default="BPE", show_default=True)
+def cli(input_dir, output_dir, limit_files, vocab_size, tokenizer_name):
+    if output_dir is None:
+        output_dir = Path(".")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    create_bpe_tokenizer(
+        input_dir=input_dir,
+        output_dir=output_dir,
+        limit_files=limit_files,
         vocab_size=vocab_size,
-        special_tokens=special_tokens,
-        show_progress=False,
+        tokenizer_name=tokenizer_name,
     )
-    # 分割大文件
-    temp_folder = "./chunks-chr1"
-    split_large_file(data_path, temp_folder, chunk_size)
 
-    # 训练分词器
-    train_tokenizer_on_chunks(tokenizer, temp_folder, trainer)
-    # 保存分词器
-    save_tokenizer(tokenizer, save_path)
-    # 在程序结束时删除临时文件夹及其内容
-    shutil.rmtree(temp_folder)
-    print(f"Temporary folder {temp_folder} deleted")
 
 if __name__ == "__main__":
-    data_path:str = "../../Datasets/Human_genome/huixin/24-txt/chr1.txt"
-    save_path:str = "./my_tokenizer-chr1/"
-    special_tokens = ["[PAD]", "[CLS]", "[SEP]", "[MASK]", "[UNK]"]
-    vocab_size:int = 2 ** 12
-    chunk_size:int = 10000000
 
-    main(data_path=data_path, 
-         save_path=save_path, 
-         special_tokens=special_tokens, 
-         vocab_size=vocab_size, 
-         chunk_size=chunk_size
-         )
+    """
+    配合的提供一个实例：
+    python tokenizer.py --input_dir "../../Datasets/Human_genome/huixin/create_corpus" --output_dir "./gena_lm_tokenizer" --vocab-size 320000 --tokenizer_name "high_level"
+    """
+    cli()
